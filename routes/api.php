69c27ec3c90ea6809f81ec6ea427f0e969ea7875
nico-php-coder <69898039+nico-php-coder@users.noreply.github.com>
2022-03-16 11:49:54 +0100

    register api route for all controller, add controller for all models.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\BookmarkController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\RedirectionController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +27,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'bookmarks' => BookmarkController::class,
+    'comments' => CommentController::class,
+    'friends' => FriendController::class,
+    'groups' => GroupController::class,
+    'redirections' => RedirectionController::class,
+    'searches' => SearchController::class,
+    'tags' => TagController::class,
+    'threads' => ThreadController::class,
+    'users' => UserController::class,
+    'votes' => VoteController::class,
+]);
