13b705f176ffc8cdc9797b9dae4c9ba50d35196a
nico-php-coder <69898039+nico-php-coder@users.noreply.github.com>
2022-04-15 17:39:17 +0200

    edit image_url value mutation when retrieve groups

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -10,16 +10,15 @@ class GroupController extends Controller
     public function getSubscribedGroups() {
       return Auth::user()->subscribedGroups->map(function ($group) {
         $owner = $group->owner;
+        $owner->image_url = "http://localhost:8000/api/ressource/avatars/$owner->alphanumeric_id";
         return [
           "alphanumeric_id" => $group->alphanumeric_id,
           "name" => $group->name,
           "image_url" => "ressource/groups/$group->alphanumeric_id",
           "owner" => $owner,
           "subscribers" => $group->subscribers->map(function ($subscriber) {
-            return [
-              'alphanumeric_id' => $subscriber->alphanumeric_id,
-              'image_url' => "ressource/avatars/$subscriber->alphanumeric_id",
-            ];
+            $subscriber->image_url = "http://localhost:8000/api/ressource/avatars/$subscriber->alphanumeric_id";
+            return $subscriber;
           }),
           "threads" => $group->threads,
         ];
@@ -28,16 +27,16 @@ class GroupController extends Controller
 
     public function getOwnedGroups() {
       return Auth::user()->ownGroups->map(function ($group) {
+        $owner = $group->owner;
+        $owner->image_url = "http://localhost:8000/api/ressource/avatars/$owner->alphanumeric_id";
         return [
           "alphanumeric_id" => $group->alphanumeric_id,
           "name" => $group->name,
           "image_url" => "ressource/groups/$group->alphanumeric_id",
-          "owner" => $group->owner,
+          "owner" => $owner,
           "subscribers" => $group->subscribers->map(function ($subscriber) {
-            return [
-              'alphanumeric_id' => $subscriber->alphanumeric_id,
-              'image_url' => "ressource/avatars/$subscriber->alphanumeric_id",
-            ];
+            $subscriber->image_url = "http://localhost:8000/api/ressource/avatars/$subscriber->alphanumeric_id";
+            return $subscriber;
           }),
           "threads" => $group->threads,
         ];
